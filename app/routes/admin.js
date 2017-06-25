bb7d9c48bc31049c6a93772fa9db258da3986d4c
Babenko <babenkosemen@gmail.com>
2017-06-25 22:30:54 +0300

    Add navigation content

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,7 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  beforeModel() {
+    const authorizated = this.controllerFor("login").get("authorizated")
+    this.set("authorizated", authorizated);
+    if(!authorizated) {
+      // this.transitionTo("login");
+    }
+  },
+
   model: function() {
     return this.store.findAll('page').then((model) => model.get("firstObject"));
+  },
+
+  setupController(currentController, model) {
+    currentController.set("authorizated", this.get("authorizated"));
+    currentController.set("model", model);
   }
+
 });
--- app/routes/admin/navigation.js
+++ app/routes/admin/navigation.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+
+  model: function() {
+    var model = this.controllerFor("admin").get("model");
+    console.log(model);
+    return model;
+  }
+
 });
