8a157d9f72f36b8fe91feb10fe391dfbd6f4e290
Babenko <babenkosemen@gmail.com>
2017-07-12 23:08:00 +0300

    Add integrity

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
-    paranoiaCheck: true
   });
   // Use `app.import` to add additional libraries to the generated
   // output files.
