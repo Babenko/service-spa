ccb62c0af6df93798b08f129b4ad7c7d883780d8
Babenko <babenkosemen@gmail.com>
2017-07-23 22:45:40 +0300

    Minor change

--- app/components/file-upload.js
+++ app/components/file-upload.js
@@ -10,17 +10,12 @@ export default EmberUploader.FileField.extend({
 
     if (!Ember.isEmpty(files)) {
       // this second argument is optional and can to be sent as extra data with the upload
-      uploader.upload(files[0]).then(data => {
-        console.log(this.get("img"), data)
-      }, error => {
-        // Handle failure
-      });
+      uploader.upload(files[0]);
     }
 
     uploader.on('didUpload', response => {
-      console.log("did", response);
-      console.log(this.get("img"))
-      this.set("img", response);
+      console.log(response, response.location);
+      this.set("img", response.location);
     });
   }
 });
